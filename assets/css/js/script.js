14a7aa4242a878408940ca04ace307226b55140a
DylanCas <djcasabona12@gmail.com>
2022-11-07 15:41:34 -0700

    Insterted starting structure for html and css layouts, to organize content

---
+++ assets/css/js/script.js
@@ -0,0 +1,2 @@
+/* Need to do; Create timer. Create start page. Create series of questions. When correct, move on, when incorrect move on and subtract time. When quiz is finished, store scores on screen. */
+
