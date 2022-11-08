a9a3d33a07bfd47370e3ad7ef53256036cf99b44
DylanCas <djcasabona12@gmail.com>
2022-11-08 13:21:15 -0700

    Starting to add variables. Pushing code to work with company on current codebase

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -1,2 +1,12 @@
 /* Need to do; Create timer. Create start page. Create series of questions. When correct, move on, when incorrect move on and subtract time. When quiz is finished, store scores on screen. */
+var timerEl = document.getElementById("countdown")
+var startBtn = document.getElementById("startBtn")
+// Create on click listener to begin the quiz and timer
 
+// create on click listener to show past scores
+
+// generate quiz content, questions and answer buttons
+
+// on click listener to record initials and score at end of quiz
+
+// on click listener for the ending go back/clear scores button
